693c732285b8a14a03f1e7c66a7293930d787295
Your Token <your-token@example.com>
2023-11-29 20:16:47 +0200

    first commit

---
+++ HW2Network2Part2/src/hw2network2part2/HW2Network2Part2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hw2network2part2;
+
+/**
+ *
+ * @author hp
+ */
+public class HW2Network2Part2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
